1f35b49d51bc4590b4f08992eb9a1069c4b1ea2e
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2026-05-09 23:51:23 +0300

    убрал ненужный импорт

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import Navbar from "../components/layout/Navbar.jsx";
 import Footer from "../components/layout/Footer.jsx";
 import HeroSection from "../components/widgets/HeroSection.jsx";
