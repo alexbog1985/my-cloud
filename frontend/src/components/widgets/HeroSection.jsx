1e8f496827fb37190a59292b5898ab023399830b
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2026-05-09 23:28:32 +0300

    header теперь в компоненте HeroSection.jsx

---
+++ frontend/src/components/widgets/HeroSection.jsx
@@ -0,0 +1,8 @@
+export default function HeroSection() {
+  return (
+      <header className="bg-primary text-white text-center py-5" style={{ borderRadius: '0 0 20px 20px' }}>
+        <h1>My Cloud</h1>
+        <p>Облачное хранилище</p>
+      </header>
+  )
+}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,16 +1,13 @@
 import { Link } from 'react-router-dom'
 import Navbar from "../components/layout/Navbar.jsx";
 import Footer from "../components/layout/Footer.jsx";
+import HeroSection from "../components/widgets/HeroSection.jsx";
 
 export default function HomePage() {
   return (
     <div className="d-flex flex-column min-vh-100">
       <Navbar />
-
-      <header className="bg-primary text-white text-center py-5" style={{ borderRadius: '0 0 20px 20px' }}>
-        <h1>My Cloud</h1>
-        <p>Облачное хранилище</p>
-      </header>
+      <HeroSection />
 
       <main className="flex-grow-1 py-5">
         <div className="container">
