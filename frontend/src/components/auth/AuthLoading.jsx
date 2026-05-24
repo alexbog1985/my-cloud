a5a8507f6f9626019e974e8738b358e30d349e9d
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2026-05-24 14:53:51 +0300

    теперь не загружает наследника пока не получил пользователя

--- frontend/src/components/auth/AuthLoading.jsx
+++ frontend/src/components/auth/AuthLoading.jsx
@@ -1,22 +1,25 @@
 import { useSelector } from 'react-redux';
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
 import { useAuth } from "../../hooks/useAuth.js";
 
 export default function AuthLoading({ children }) {
   const { fetchUser } = useAuth();
   const loading = useSelector(state => state.auth.loading);
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  const [authChecked, setAuthChecked] = useState(false);
 
   useEffect(() => {
     if (!isAuthenticated && !loading) {
       const token = localStorage.getItem("token");
       if (token) {
-        fetchUser().then();
+        fetchUser().finally(() => setAuthChecked(true));
+      } else {
+        setAuthChecked(true);
       }
     }
-  }, [isAuthenticated, loading, fetchUser]);
+  }, [isAuthenticated, loading, authChecked, fetchUser]);
 
-  if (loading) {
+  if (!authChecked || loading) {
     return (
       <div className="d-flex justify-content-center align-items-center" style={{height: '100vh'}}>
         <div className="spinner-border text-primary" role="status">
