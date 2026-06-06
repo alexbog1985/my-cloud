a495ad0edfecd19b94370fc96f9b0c59482f57cd
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2026-06-06 18:01:07 +0300

    небольшое улучшение

--- backend/files/permissions.py
+++ backend/files/permissions.py
@@ -1,7 +1,12 @@
 from rest_framework import permissions
 
+
 class IsOwnerOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        if request.user is None or not request.user.is_authenticated:
+            return False
+
         if request.user.is_admin:
             return True
+
         return obj.user == request.user
