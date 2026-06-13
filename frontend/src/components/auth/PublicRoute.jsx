70887363a3303948e3a619020ecc50639e4509a6
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2026-06-13 13:00:01 +0300

    исправлен

--- frontend/src/components/auth/PublicRoute.jsx
+++ frontend/src/components/auth/PublicRoute.jsx
@@ -2,6 +2,6 @@ import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 export default function PublicRoute({ children }) {
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  const { isAuthenticated } = useSelector(state => state.auth);
   return !isAuthenticated ? children : <Navigate to="/files" />;
 }
