37b60bb20642eb1266c52ad1a36b7cdbd70ee725
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2026-06-19 12:51:50 +0300

    директория в темплейтах

--- backend/mycloud/settings.py
+++ backend/mycloud/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'mycloud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, BASE_DIR.parent / 'frontend' / 'dist'],
+        'DIRS': [BASE_DIR, BASE_DIR.parent / 'staticfiles'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
